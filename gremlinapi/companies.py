3b1a93f68dd1d61275fc270e915c075610968520
Kyle Hultman <khultman@gmail.com>
2020-03-18 13:56:29 -0400

    refactoring required parameter checks in companies endpoint

--- gremlinapi/companies.py
+++ gremlinapi/companies.py
@@ -23,13 +23,27 @@ log = logging.getLogger('GremlinAPI.client')
 class GremlinAPICompanies(GremlinAPI):
 
     @classmethod
-    @register_cli_action('get_company', ('identifier',), ('',))
-    def get_company(cls, https_client=get_gremlin_httpclient(), **kwargs):
+    def _error_if_not_identifier(cls, **kwargs):
         identifier = kwargs.get('identifier', None)
         if not identifier:
             error_msg = f'Company identifier not supplied correctly: {kwargs}'
             log.fatal(error_msg)
             raise GremlinParameterError(error_msg)
+        return identifier
+
+    @classmethod
+    def _error_if_not_email(cls, **kwargs):
+        email = kwargs.get('email', None)
+        if not email:
+            error_msg = f'No email address provided: {kwargs}'
+            log.fatal(error_msg)
+            raise GremlinParameterError(error_msg)
+        return email
+
+    @classmethod
+    @register_cli_action('get_company', ('identifier',), ('',))
+    def get_company(cls, https_client=get_gremlin_httpclient(), **kwargs):
+        identifier = cls._error_if_not_identifier(**kwargs)
         endpoint = f'/companies/{identifier}'
         method = 'GET'
         headers = https_client.header()
@@ -39,11 +53,7 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('list_company_clients', ('identifier',), ('',))
     def list_company_clients(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
         endpoint = f'/companies/{identifier}/clients'
         method = 'GET'
         headers = https_client.header()
@@ -53,16 +63,8 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('invite_company_user', ('identifier', 'body'), ('',))
     def invite_company_user(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
-        data = kwargs.get('body', None)
-        if not data:
-            error_msg = f'No invite body supplied: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
+        data = cls._error_if_not_body(**kwargs)
         endpoint = f'/companies/{identifier}/invites'
         method = 'POST'
         headers = https_client.header()
@@ -72,16 +74,8 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('delete_company_invite', ('identifier', 'email'), ('',))
     def delete_company_invite(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
-        email = kwargs.get('email', None)
-        if not email:
-            error_msg = f'No email address provided: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
+        email = cls._error_if_not_email(**kwargs)
         endpoint = f'/companies/{identifier}/invites/{email}'
         method = 'DELETE'
         headers = https_client.header()
@@ -91,11 +85,7 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('company_mfa_prefs', ('identifier',), ('forceMfa', 'mfaProviders', 'defaultMfaProvider',))
     def company_mfa_prefs(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
         endpoint = f'/companies/{identifier}/mfaPrefs'
         method = 'POST'
         headers = https_client.header()
@@ -111,11 +101,7 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('update_company_prefs', ('identifier',), ('domain',))
     def update_company_prefs(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
         endpoint = f'/companies/{identifier}/prefs'
         method = 'POST'
         headers = https_client.header()
@@ -129,11 +115,7 @@ class GremlinAPICompanies(GremlinAPI):
                          ('identifier',),
                          ('enabled', 'entityId', 'idpUrl', 'certificate', 'forced'))
     def update_company_saml_props(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
         endpoint = f'/companies/{identifier}/saml/props'
         method = 'POST'
         headers = https_client.header()
@@ -151,11 +133,7 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('list_company_users', ('identifier',), ('',))
     def list_company_users(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
         endpoint = f'/companies/{identifier}/users'
         method = 'GET'
         headers = https_client.header()
@@ -165,36 +143,23 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('list_company_users', ('identifier', 'email',), ('body',))
     def update_company_user_role(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
-        email = kwargs.get('email', None)
-        if not email:
-            error_msg = f'User email not provided: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
+        email = cls._error_if_not_email(**kwargs)
         endpoint = f'​/companies​/{identifier}​/users​/{email}'
         method = 'PUT'
         headers = https_client.header()
-        data = kwargs.get('body')
-        (resp, body) = https_client.api_call(method, endpoint, **{'body': data, 'headers': headers})
+        data = kwargs.get('body', None)
+        if data:
+            (resp, body) = https_client.api_call(method, endpoint, **{'body': data, 'headers': headers})
+        else:
+            (resp, body) = https_client.api_call(method, endpoint, **{'headers': headers})
         return body
 
     @classmethod
     @register_cli_action('activate_company_user', ('identifier', 'email',), ('',))
     def activate_company_user(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
-        email = kwargs.get('email', None)
-        if not email:
-            error_msg = f'User email not provided: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
+        email = cls._error_if_not_email(**kwargs)
         endpoint = f'​/companies​/{identifier}​/users​/{email}​/active'
         method = 'POST'
         headers = https_client.header()
@@ -204,16 +169,8 @@ class GremlinAPICompanies(GremlinAPI):
     @classmethod
     @register_cli_action('deactivate_company_user', ('identifier', 'email',), ('',))
     def deactivate_company_user(cls, https_client=get_gremlin_httpclient(), **kwargs):
-        identifier = kwargs.get('identifier', None)
-        if not identifier:
-            error_msg = f'Company identifier not supplied correctly: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
-        email = kwargs.get('email', None)
-        if not email:
-            error_msg = f'User email not provided: {kwargs}'
-            log.fatal(error_msg)
-            raise GremlinParameterError(error_msg)
+        identifier = cls._error_if_not_identifier(**kwargs)
+        email = cls._error_if_not_email(**kwargs)
         endpoint = f'​/companies​/{identifier}​/users​/{email}​/active'
         method = 'DELETE'
         headers = https_client.header()
