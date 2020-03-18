c279722ba7aa2f4d247842be8578b0507430f482
Kyle Hultman <khultman@gmail.com>
2020-03-18 16:59:54 -0400

    prototyped templates endpoint

--- gremlinapi/templates.py
+++ gremlinapi/templates.py
@@ -21,4 +21,70 @@ log = logging.getLogger('GremlinAPI.client')
 
 
 class GremlinAPITemplates(GremlinAPI):
-    pass
+
+    @classmethod
+    @register_cli_action('list_templates', ('',), ('teamId',))
+    def list_templates(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'GET'
+        endpoint = cls._optional_team_endpoint(f'/templates', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header()})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
+    @classmethod
+    @register_cli_action('create_template', ('body',), ('teamId',))
+    def create_template(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'POST'
+        data = cls._error_if_not_body(**kwargs)
+        endpoint = cls._optional_team_endpoint(f'/templates', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header(), 'body': data})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
+    @classmethod
+    @register_cli_action('get_template', ('guid',), ('teamId',))
+    def get_template(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'GET'
+        guid = cls._error_if_not_guid(**kwargs)
+        endpoint = cls._optional_team_endpoint(f'/templates/{guid}', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header()})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
+    @classmethod
+    @register_cli_action('delete_template', ('guid',), ('teamId',))
+    def delete_template(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'DELETE'
+        guid = cls._error_if_not_guid(**kwargs)
+        endpoint = cls._optional_team_endpoint(f'/templates/{guid}', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header()})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
+    @classmethod
+    @register_cli_action('list_command_templates', ('',), ('teamId',))
+    def list_command_templates(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'GET'
+        endpoint = cls._optional_team_endpoint(f'/templates/command', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header()})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
+    @classmethod
+    @register_cli_action('list_target_templates', ('',), ('teamId',))
+    def list_target_templates(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'GET'
+        endpoint = cls._optional_team_endpoint(f'/templates/target', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header()})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
+    @classmethod
+    @register_cli_action('list_trigger_templates', ('',), ('teamId',))
+    def list_trigger_templates(cls, https_client=get_gremlin_httpclient(), *args, **kwargs):
+        method = 'GET'
+        endpoint = cls._optional_team_endpoint(f'/templates/trigger', **kwargs)
+        payload = cls._payload(**{'headers': https_client.header()})
+        (resp, body) = https_client.api_call(method, endpoint, **payload)
+        return body
+
