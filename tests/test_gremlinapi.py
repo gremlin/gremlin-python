2e6ac50403b2c219688059dab6843b6a7a4e9a0b
Kyle Hultman <khultman@gmail.com>
2020-03-20 11:30:03 -0400

    api_key test case

--- tests/test_gremlinapi.py
+++ tests/test_gremlinapi.py
@@ -2,10 +2,13 @@
 #
 # Copyright (C) 2020 Kyle Hultman <kyle@gremlin.com>, Gremlin Inc <sales@gremlin.com>
 
+import json
 import os
 import pickle
 import tempfile
-import json
+import unittest
+
 
 from gremlinapi import *
+
 
---
+++ tests/test_httpclient.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Kyle Hultman <kyle@gremlin.com>, Gremlin Inc <sales@gremlin.com>
+
+import json
+import os
+import pickle
+import sys
+import tempfile
+import unittest
+
+
+parentPath = os.path.abspath("..")
+if parentPath not in sys.path:
+    sys.path.insert(0, parentPath)
+
+from gremlinapi.config import GremlinAPIConfig as config
+from gremlinapi.http_clients import get_gremlin_httpclient
+
+api_key = 'api-key-string'
+bearer_token = 'bearer-token-string'
+
+class TestHttpClient(unittest.TestCase):
+
+    def test_api_key(self):
+        config.api_key = api_key
+        https_client = get_gremlin_httpclient()
+        header = https_client.header()
+        self.assertIn(api_key, header['Authorization'])
+
+
+if __name__ == '__main__':
+    unittest.main()
