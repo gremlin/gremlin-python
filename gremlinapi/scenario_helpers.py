052c407700f1bff213860066ad11715a039cfa67
Kyle Hultman <khultman@gmail.com>
2020-04-13 15:24:30 -0400

    scenario helper structure starting to shape up

--- gremlinapi/scenario_helpers.py
+++ gremlinapi/scenario_helpers.py
@@ -12,6 +12,7 @@ from gremlinapi.exceptions import (
     GremlinParameterError
 )
 
+from gremlinapi.attack_helpers import GremlinAttackCommandHelper, GremlinAttackTargetHelper
 from gremlinapi.clients import GremlinAPIClients as clients
 from gremlinapi.containers import GremlinAPIContainers as containers
 from gremlinapi.providers import GremlinAPIProviders as providers
@@ -22,9 +23,16 @@ class GremlinScenarioHelper(object):
     def __init__(self, *args, **kwargs):
         self._description = str()
         self._hypothesis = str()
-        self._steps = []
+        self._steps = list()
         self.description = kwargs.get('description', None)
         self.hypothesis = kwargs.get('hypothesis', None)
+
+    def add_step(self, *args, **kwargs):
+        _step = kwargs.get('step', None)
+        if not issubclass(_step, GremlinScenarioStep):
+            error_msg = f'The step must extend from {type(GremlinScenarioStep)}'
+            log.fatal(error_msg)
+            raise GremlinParameterError(error_msg)
 
     @property
     def description(self):
@@ -48,3 +56,78 @@ class GremlinScenarioHelper(object):
             'hypothesis': self._hypothesis
         }
         return json.dumps(model)
+
+
+class GremlinScenarioStep(object):
+    def __init__(self, *args, **kwargs):
+        self._delay = 5
+
+    @property
+    def delay(self):
+        return self._delay
+
+    @delay.setter
+    def delay(self, _delay):
+        if isinstance(_delay, int) and _delay >= 1:
+            self._delay = _delay
+        else:
+            error_msg = f'delay expects a positive integer {type(int)}'
+            log.fatal(error_msg)
+            raise GremlinParameterError(error_msg)
+
+    def __repr__(self):
+        model = dict()
+        return json.dumps(model)
+
+
+class GremlinILFIStep(object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._command = GremlinAttackCommandHelper()
+        self._target = None
+        self.target = kwargs.get('target', None)
+
+    def add_attack(self, *args, **kwargs):
+        atk_helper = kwargs.get('attack', None)
+
+    @property
+    def command(self):
+        return self._command
+
+    @command.setter
+    def command(self, _command):
+        if issubclass(_command, GremlinAttackCommandHelper):
+            self._command = _command
+        else:
+            error_msg = f'command expects a GremlinAttackCommandHelper {type(GremlinAttackCommandHelper)}'
+            log.fatal(error_msg)
+            raise GremlinParameterError(error_msg)
+
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, _target=None):
+        if issubclass(_target, GremlinAttackTargetHelper):
+            self._target = _target
+        else:
+            error_msg = f'target expects a GremlinAttackTargetHelper {type(GremlinAttackTargetHelper)}'
+            log.fatal(error_msg)
+            raise GremlinParameterError(error_msg)
+
+    def __repr__(self):
+        model = json.loads(super().__repr__())
+
+        return json.dumps(model)
+
+
+class GremlinALFIStep(object):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def __repr__(self):
+        model = json.loads(super().__repr__())
+
+        return json.dumps(model)
+
