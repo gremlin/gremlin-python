bff6293fc92b48573bc2d703c0e0de80f81de5ab
Kyle Bouchard <kyle.bouchard@gremlin.com>
2021-03-11 19:18:04 -0500

    Added properly mocking get request to test_attacks to test out list_attacks. This test covers the code in http_clients.api_call as well.

--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -3,46 +3,23 @@
 # Copyright (C) 2021 Kyle Bouchard <kyle.bouchard@gremlin.com>, Gremlin Inc <sales@gremlin.com>
 
 import unittest
+import json
+import requests
 from unittest.mock import patch
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock, Mock, PropertyMock
 import gremlinapi
 from gremlinapi.attacks import GremlinAPIAttacks
 import gremlinapi.http_clients
 
 class TestAttacks(unittest.TestCase):
-
-    # @patch('users.requests.get')  # Mock 'requests' module 'get' method.
-    # def test_request_response_with_decorator(self, mock_get):
-    #     """Mocking using a decorator"""
-    #     mock_get.return_value.status_code = 200 # Mock status code of response.
-    #     response = get_users()
-
-    #     # Assert that the request-response cycle completed successfully.
-    #     self.assertEqual(response.status_code, 200)
-
-    @patch('gremlinapi.http_clients.requests.get')
+    @patch('requests.get')
     def test_list_attacks_with_decorator(self, mock_get) -> None:
-        testbody = {"testkey":"testval"}
-        attackObj = GremlinAPIAttacks()
-        attackObj.list_attacks = MagicMock(return_value=testbody)
-        returnval = attackObj.list_attacks()
-        self.assertEqual(returnval,testbody)
+        mock_data = {"testkey":"testval"}
+        def mock_json():
+            return mock_data
 
-        # apiObj = gremlinapi.http_clients
-        # apiObj.api_call = MagicMock(return_value = (None, testbody))
-        # returnval = apiObj.api_call()
+        mock_get.return_value = requests.Response()
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json = mock_json
 
-        # # myMock = Mock(return_value=testbody,status_code=200)
-        # mock_get.list_attacks.return_value.body = testbody
-        # mock_get.return_value.status_code = 200
-        # # myval.status_code: int = 200
-        # # gremlinapi.login(email=GREMLIN_USER,
-        # #          password=GREMLIN_PASSWORD,
-        # #          company_name=GREMLIN_COMPANY,
-        # #          token="")
-        # myreturn = GremlinAPIAttacks.list_attacks()
-        # mock_get.assert_called_with(testbody)
-        # # # testreturn = gAttacks
-        # # gAttacks.assert_called_with(testbody)
-
-        # # self.assertEqual(gAttacks.status_code, 200)
+        self.assertEqual(GremlinAPIAttacks.list_attacks(),mock_data)
