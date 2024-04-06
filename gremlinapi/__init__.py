350e9783251a3be742cf916dc0d3be520f7e591e
Marcello Romani <marcello.romani.enit@gmail.com>
2024-04-06 09:57:48 +0100

    clearer error message if one or both params are missing

--- gremlinapi/__init__.py
+++ gremlinapi/__init__.py
@@ -239,7 +239,7 @@ def saml_login(email=GremlinAPIConfig.user, saml_assertion=None, relay_state=Non
             )
         GremlinAPIConfig.user = email
     if not saml_assertion or not relay_state:
-        error_msg = "Expecting a SAML assertion and relay state, received none"
+        error_msg = "Both saml_assertion and relay_state arguments must be specified"
         log.fatal(error_msg)
         raise GremlinParameterError(error_msg)
     acs_response = GremlinAPISaml.acs(
